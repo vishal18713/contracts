cc3f4300a0cae2d84e60bd604df4acde854f04f7
vishal18713 <lcs2023052@iiitl.ac.in>
2024-10-25 22:47:49 +0530

    feat: Add NFT collection and token management contracts with fractional ownership

---
+++ test_2.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+
+contract MyToken is ERC1155, Ownable, ERC1155Burnable {
+    constructor(
+        address initialOwner
+    )
+        ERC1155("https://emerald-managerial-firefly-535.mypinata.cloud/ipfs/")
+        Ownable(initialOwner)
+    {}
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount) public {
+        _mint(account, id, amount, "");
+    }
+
+    function uri(uint _tokenid) public pure override returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "https://emerald-managerial-firefly-535.mypinata.cloud/ipfs/",
+                    Strings.toString(_tokenid),
+                    ".json"
+                )
+            );
+    }
+}
